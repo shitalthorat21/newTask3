745687daf4bba407813e069cf7877f15d23b054b
shitalthorat21 <adviarvi2102@gmail.com>
2020-03-22 19:46:00 +0530

    final

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const User=mongoose.model('User');
 router.get('/add', userController.addUser);
 router.post('/add', userController.addUserpost);
 
+
 router.get('/users', (req,res)=>{
     User.find((err,docs)=>{
         if(!err){
@@ -18,6 +19,38 @@ router.get('/users', (req,res)=>{
             console.log("Error in users");
         }
     });
+});
+
+router.get("/:email", function (req,res)
+{
+    let email=req.params.email;
+    var data={
+        name:req.body.name,
+        age:req.body.age,
+        city:req.body.city,
+        state:req.body.state,
+    }
+        User.findByEmail(email, data, function (err,doc){
+        if(!err){
+            res.render("add", {
+                viewTitle:"Update User",
+                users:doc
+            });
+        }
+    });
+});
+
+router.get('/:email', function(req, res) {
+	console.log(req.params.email);
+	let email = req.params.email;
+	User.findByEmail(req.params.email, function(err,doc){	
+	
+		if (!err)
+			res.redirect("users");
+		else
+            res.send('Error');
+    	
+	});
 });
 
 
