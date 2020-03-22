54c7f49ad717e033ecfdc92f7e2d7550e84a7c60
shitalthorat21 <adviarvi2102@gmail.com>
2020-03-22 16:02:59 +0530

    fourthcommit

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose=require('mongoose');
 const router = express.Router();
 const userController=require('./userController');
-
+const User=mongoose.model('User');
 
 router.get('/add', userController.addUser);
 router.post('/add', userController.addUserpost);
+
+router.get('/users', (req,res)=>{
+    User.find((err,docs)=>{
+        if(!err){
+            res.render("users",{
+                users:docs
+            });
+        }
+        else{
+            console.log("Error in users");
+        }
+    });
+});
 
 
 
--- userController.js
+++ userController.js
@@ -8,7 +8,22 @@ exports.addUser=(req,res)=>{
     res.render("add",{title:"AddUser"});
 }
 exports.addUserpost=(req,res)=>{
-    console.log("Hi");
+    var user=new User();
+    user.name=req.body.name;
+    user.age=req.body.age;
+    user.city=req.body.city;
+    user.state=req.body.state;
+    user.email=req.body.email;
+    user.save((err,doc)=>
+    {
+        if(!err){
+            res.redirect("users")
+        }
+        else{
+            res.send("error occured");
+        }
+    });
+    
 }
 
 
